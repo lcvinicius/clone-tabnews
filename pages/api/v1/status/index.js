6b24e6dc2240108ab340d411fc5604bee68e56ee
Lucas Vinícius <lucasvsn2003@gmail.com>
2024-12-05 16:57:30 +0000

    add `jsconfig.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
+  const result = await database.query("SELECT 18 + 1;");
   console.log(result);
   response.status(200).json({ chave: "são acima da média" });
 }
